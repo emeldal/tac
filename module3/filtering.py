1c268cca146fbcc3113ffd24fc814c0bf0746882
emeldal <72862831+emeldal@users.noreply.github.com>
2020-11-04 21:44:28 +0100

    Update filtering.py

--- module3/filtering.py
+++ module3/filtering.py
@@ -9,7 +9,8 @@ sw += ["les", "plus", "cette", "fait", "faire", "être", "deux", "comme", "dont"
        "ils", "bien", "sans", "peut", "tous", "après", "ainsi", "donc", "cet", "sous",
        "celle", "entre", "encore", "toutes", "pendant", "moins", "dire", "cela", "non",
        "faut", "trois", "aussi", "dit", "avoir", "doit", "contre", "depuis", "autres",
-       "van", "het", "autre", "jusqu"]
+       "van", "het", "autre", "jusqu", "vers", "quelque", "car", "quel", "abord", "parce", "leurs", "chez",
+       "aucun", "alors", "auquel", "tandis", "quand", "devons", "donne", "telles"]
 sw = set(sw)
 
 
