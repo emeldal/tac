4698fec89a9c1edbd1816107a461ba089b4e41da
emeldal <72862831+emeldal@users.noreply.github.com>
2020-11-06 13:22:57 +0100

    new

--- module3/s2_wordcloud.sh
+++ module3/s2_wordcloud.sh
@@ -2,7 +2,7 @@
 
 # Building a wordcloud based on one year of bulletins
 
-YEAR=$1
+YEAR=$11857
 cat data/txt/*_${YEAR}_*.txt > module3/${YEAR}.txt
 python module3/filter.py $YEAR
 wordcloud_cli --text module3/${YEAR}_keywords.txt --imagefile module3/${YEAR}.png --width 2000 --height 1000
