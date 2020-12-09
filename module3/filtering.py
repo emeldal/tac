5bee9b93df3e9a79adec12742bc927474e914feb
emeldal <72862831+emeldal@users.noreply.github.com>
2020-12-09 12:17:36 +0100

    test

--- module3/filtering.py
+++ module3/filtering.py
@@ -1,4 +1,3 @@
-
 """Filter out stopwords for word cloud"""
 
 import sys
@@ -14,22 +13,25 @@ sw += ["les", "plus", "cette", "fait", "faire", "être", "deux", "comme", "dont"
        "ils", "bien", "sans", "peut", "tous", "après", "ainsi", "donc", "cet", "sous",
        "celle", "entre", "encore", "toutes", "pendant", "moins", "dire", "cela", "non",
        "faut", "trois", "aussi", "dit", "avoir", "doit", "contre", "depuis", "autres",
-       "van", "het", "autre", "jusqu", "vers", "quelque", "car", "quel", "abord", "parce", 
+       "van", "het", "autre", "jusqu", "ville", "vers", "quelque", "car", "quel", "abord", "parce", 
        "leurs", "chez", "aucun", "alors", "auquel", "tandis", "quand", "devons", "donne", 
        "telles", "toujours", "seulement", "pourra", "cependant", "plusieurs", "elles", 
        "déjà", "très", "lorsque", "trouve", "crois", "toute", "faite", "ans", "celles"
        "celui", "quelques", "laquelle", "faites", "tel", "etc", "devront", "chaque", "agit"
        "mettre", "ceux", "pourront", "avant", "prendre", "aucune", "peu", "fera", "également"
-       "celles", "dessus", "devant", "beaucoup"]
+       "celles", "dessus", "devant", "beaucoup", "rue", "échevin", "echevin"]
 sw = set(sw)
 
 
-def filtering(dpath, year):
-    path = f"{dpath}/{year}.txt"
-    output = open(f"{dpath}/{year}_keywords.txt", "w", encoding='utf-8')
-
-    with open(path, encoding='utf-8') as f:
-
+def filtering(year, folder=None):
+    if folder is None:
+        input_path = f"{year}.txt"
+        output_path = f"{year}_keywords.txt"
+    else:
+        input_path = f"{folder}/{year}.txt"
+        output_path = f"{folder}/{year}_keywords.txt"
+    output = open(output_path, "w", encoding='utf-8')
+    with open(input_path, encoding='utf-8') as f:
         text = f.read()
         words = nltk.wordpunct_tokenize(text)
         kept = [w.lower() for w in words if len(
@@ -42,6 +44,4 @@ def filtering(dpath, year):
 if __name__ == '__main__':
     data_path = sys.argv[1]
     chosen_year = sys.argv[2]
-
     filtering(data_path, chosen_year)
-
